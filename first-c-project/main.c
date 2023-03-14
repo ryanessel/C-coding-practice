8aa021ab344daf205bd97e218448e39a0c9f24ec
Ry Work <spucti19@gmail.com>
2023-03-14 10:38:33 -0400

    first simple C projects

---
+++ first-c-project/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //\n adds a space under the print
+    printf("Hello wodsfdsfsd!\n");
+    int firstNumber = 12;
+    int secondNumber = 32;
+    char testMesage[] = "It's a beautiful day outside. \nBirds are blooming, flowers are singing. . .\nOn days like these. . .";
+    return printf(testMesage), firstNumber + secondNumber;
+}
+
